6e45fd8287ab56efb4cdd32b26179da080567106
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-06-17 11:17:25 -0500

    commit tree 532b7a6f7ce293255a7cd77004b819726a5e8170

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(arrayOfObject){
+    const found = arrayOfObject.find(object=>object.result==="W")
+    if(found===undefined){
+        return found
+    }else{
+        return found.year
+    }
+
+}
+
